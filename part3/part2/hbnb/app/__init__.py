795873473e1b60e3746d5c15aef7bc97a97bdd4d
SaraEstelle <12152@holbertonstudents.com>
2026-03-09 13:58:09 +0100

    Modifiying the App Factiry to include Configuration

--- part3/part2/hbnb/app/__init__.py
+++ part3/part2/hbnb/app/__init__.py
@@ -6,26 +6,25 @@ for the HBnB project.
 """
 
 from flask import Flask
-from config import config
 from app.api import api_bp
 
 
-def create_app(config_name="default"):
+def create_app(config_class="config.DevelopmentConfig"):
     """
     Application factory function.
 
     Args:
-        config_name (str): Configuration profile name.
+        config_class (str): Dotted path to the configuration class.
 
     Returns:
         Flask: Configured Flask application instance.
     """
     app = Flask(__name__)
 
+    app.config.from_object(config_class)
+
     from app.services import facade
     facade.reset()
-
-    app.config.from_object(config[config_name])
 
     # Register API Blueprint
     app.register_blueprint(api_bp)
--- part3/part2/hbnb/config.py
+++ part3/part2/hbnb/config.py
@@ -9,7 +9,5 @@ class DevelopmentConfig(Config):
     """Development configuration."""
     DEBUG = True
 
-config = {
-    'development': DevelopmentConfig,
-    'default': DevelopmentConfig
-}
+# DevelopmentConfig est utilisé par défaut dans create_app()
+# via : app.config.from_object("config.developmentConfig")
